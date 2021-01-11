82be33851e08719a4397490413141ce027c16042
Gonen <davidogonen@gmail.com>
2021-01-11 21:08:09 +0200

    commit test

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ if __name__ == '__main__':
     se = search_engine_best.SearchEngine()
     se.build_index_from_parquet("C:\\Users\\david\\PycharmProjects\\Search_Engine\\data\\benchmark_data_train.snappy.parquet")
     print(se.search("30-62000"))
-    print("commit")
+
 #"C:\\Users\\david\\Search_Engine\\testData"
 #C:\\Users\\david\\PycharmProjects\\Search_Engine-master\\testData
