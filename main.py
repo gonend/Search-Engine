3e3494182d5787f051c774e05b64e93da7b9b50d
gonend <48886619+gonend@users.noreply.github.com>
2022-01-22 17:16:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import search_engine_best
 
 if __name__ == '__main__':
     se = search_engine_best.SearchEngine()
-    se.build_index_from_parquet("C:\\Users\\david\\PycharmProjects\\Search_Engine\\data\\benchmark_data_train.snappy.parquet")
+    se.build_index_from_parquet("..\\data\\benchmark_data_train.snappy.parquet")
     print(se.search("30-62000"))
 
 #"C:\\Users\\david\\Search_Engine\\testData"
